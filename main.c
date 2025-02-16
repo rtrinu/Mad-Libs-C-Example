8ad11640dc87f062107378c9928650e30f6a6471
rtrinu <rbinu2004@gmail.com>
2025-02-16 12:02:31 +0000

    Final Commit, first tutorial project completed

--- main.c
+++ main.c
@@ -1,6 +1,21 @@
 #include <stdio.h>
 
 int main(void) {
-    printf("Hello, World!\n");
+
+    char color[20];
+    char noun[20];
+    char fname[20];
+    char lname[20];
+    printf("Please enter a color\n");
+    scanf("%s", color);
+    printf("Please enter a noun\n");
+    scanf("%s", noun);
+    printf("Please enter a name\n");
+    scanf("%s%s", fname,lname);
+
+    printf("Roses are %s\n", color);
+    printf("Violets are blue\n");
+    printf("I love %s %s\n",fname,lname);
+
     return 0;
 }
